ec89b54de145b5891d131f4955a232e4d3fb05c1
Terry Chen <terrychen@fb.com>
2022-01-05 19:51:16 -0800

    fix quantize bn

--- torch/nn/quantized/modules/batchnorm.py
+++ torch/nn/quantized/modules/batchnorm.py
@@ -6,12 +6,12 @@ class BatchNorm2d(torch.nn.BatchNorm2d):
     r"""This is the quantized version of :class:`~torch.nn.BatchNorm2d`.
     """
 
-    def __init__(self, num_features, eps=1e-5, momentum=0.1, device=None, dtype=None) -> None:
+    def __init__(self, num_features, scale=1.0, zero_point=0, eps=1e-5, momentum=0.1, device=None, dtype=None) -> None:
         factory_kwargs = {'device': device, 'dtype': dtype}
         super(BatchNorm2d, self).__init__(num_features, **factory_kwargs)
         self.eps = eps
-        self.scale = 1.0
-        self.zero_point = 0
+        self.register_buffer('scale', torch.tensor(scale, **factory_kwargs))
+        self.register_buffer('zero_point', torch.tensor(zero_point, **factory_kwargs))
 
     def forward(self, input):
         return torch.ops.quantized.batch_norm2d(input, self.weight, self.bias, self.running_mean,
@@ -31,8 +31,8 @@ class BatchNorm2d(torch.nn.BatchNorm2d):
         new_mod.bias = mod.bias
         new_mod.running_mean = mod.running_mean
         new_mod.running_var = mod.running_var
-        new_mod.scale = float(scale)
-        new_mod.zero_point = int(zero_point)
+        new_mod.scale = scale
+        new_mod.zero_point = zero_point
         return new_mod
 
 # TODO: dedup with BatchNorm2d
